a9f1f6b5f806359ad7d08e67969fa7c66e4d95d5
greg114 <greg_114@163.com>
2025-07-27 09:14:15 +0800

    1

--- sanedit/dbg/warrior_anlysis.py
+++
@@ -1,6 +0,0 @@
-path = r'D:\data\Code\dbg\sanedit\warriors\ea0b.txt'
-with open(path,'r',encoding='utf-8') as f:
-    content = f.read().replace('\n','')
-    t = content[122:130]
-    print(t)
-    ss=0
